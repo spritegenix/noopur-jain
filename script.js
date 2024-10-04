3bb593b7ea8181ab6c93dccc5ea3f5284de7fc3e
pankaj <er.pankaj,k4@gmail.com>
2024-10-04 17:07:21 +0530

    fixes

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ menuButton.addEventListener("click", () => {
 coursesButton.addEventListener("click", () => {
   coursesMenu.classList.toggle("hidden");
 });
+
+function scrollWithOffset(event) {
+  event.preventDefault(); // Prevent default anchor behavior
+  const targetId = event.currentTarget.getAttribute("href"); // Get target id from href
+  const targetElement = document.querySelector(targetId); // Get the target element
+
+  if (targetElement) {
+    const offsetPosition =
+      targetElement.getBoundingClientRect().top + window.scrollY - 150;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  }
+}
+
+function openModal(image) {
+  const modal = document.getElementById("imageModal");
+  const modalImage = document.getElementById("modalImage");
+  modalImage.src = image.src; // Set the image source in the modal
+  modal.classList.remove("hidden"); // Show the modal
+}
+
+function closeModal() {
+  const modal = document.getElementById("imageModal");
+  modal.classList.add("hidden"); // Hide the modal
+}
